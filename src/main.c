1b983d3879642f8f425abcc22acc4e1ba67db93a
ZuzannaZawartka <raf.zuzia7@gmail.com>
2025-01-08 14:28:23 +0100

    refactor: Simplified main.c by delegating responsibilities to cashier.c and customer.c
    Refactored main.c to improve modularity and reduce complexity by moving cashier-related functions to cashier.c and customer-related functions to customer.c

--- src/main.c
+++ src/main.c
@@ -12,10 +12,7 @@
 #define NUM_CUSTOMERS 5
 
 // Zmienna globalna do sygnalizowania zakończenia pracy
-extern int should_exit;
-
-// Tablica przechowująca PID procesów klientów (tymczasowo) narazie aby usuwac procesy klientów.
-pid_t customer_pids[NUM_CUSTOMERS];
+extern int should_exit=0;
 
 // Handler dla sygnału SIGINT (Ctrl+C)
 void sigint_handler(int signum) {
@@ -24,51 +21,17 @@ void sigint_handler(int signum) {
     // Ustawienie flagi zakończenia
     should_exit = 1;
     
-    // Zabijanie wszystkich procesów klientów
-    for (int i = 0; i < NUM_CUSTOMERS; i++) {
-        if (customer_pids[i] > 0) {
-            kill(customer_pids[i], SIGTERM);  // Wysyłanie sygnału zakończenia do klienta
-        }
-    }
+    terminate_all_customers();
 
     // Kasjerzy zakończą swoje wątki po sygnale
     printf("Wszystkie procesy i wątki zostaną zakończone.\n");
 }
 
-// Tworzenie procesów klientów
-void create_customer_process(int customer_id, int cashier_id) {
-    pid_t pid = fork();
-    
-    if (pid == 0) { 
-        customer_function(&cashier_id);  // Klient działa, przypisany do danego kasjera
-        exit(0); // Zakończenie procesu klienta
-    } else if (pid < 0) {
-        perror("Błąd tworzenia procesu klienta");
-        exit(1);
-    }
-
-    // Zapisz PID klienta
-    customer_pids[customer_id] = pid;
-}
-
-// Funkcja czekająca na zakończenie procesów klientów
-void wait_for_customers(int num_customers) {
-    for (int i = 0; i < num_customers; i++) {
-        wait(NULL);  // Czeka na zakończenie każdego procesu klienta
-    }
-}
-
-// Funkcja do oczekiwania na zakończenie pracy kasjerów
-void wait_for_cashiers(pthread_t* cashier_threads, int num_cashiers) {
-    for (int i = 0; i < num_cashiers; i++) {
-        pthread_join(cashier_threads[i], NULL);  // Czeka na zakończenie każdego wątku kasjera
-    }
-}
 
 int main() {
     pthread_t cashier_threads[NUM_CASHIERS];
     int cashier_ids[NUM_CASHIERS];
-    
+
     // Rejestracja handlera sygnału SIGINT
     signal(SIGINT, sigint_handler);
 
@@ -76,10 +39,7 @@ int main() {
     init_cashiers(cashier_threads, cashier_ids, NUM_CASHIERS);
 
     // Tworzenie procesów klientów
-    for (int i = 0; i < NUM_CUSTOMERS; i++) {
-        int customer_cashier_id = rand() % NUM_CASHIERS + 1;  // Losowy kasjer dla klienta
-        create_customer_process(i, customer_cashier_id);  // Tworzenie procesu klienta
-    }
+    create_customer_processes(NUM_CUSTOMERS, NUM_CASHIERS);
 
     // Czekanie na zakończenie procesów klientów
     wait_for_customers(NUM_CUSTOMERS);
