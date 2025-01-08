886b8ccd7d024b0892911fc54e123a27bbfa0cb3
ZuzannaZawartka <raf.zuzia7@gmail.com>
2025-01-08 23:44:51 +0100

    feat: Update main function with shared memory handling
    - Introduced dynamic handling for the number of customers and cashiers with configurable values.
    - Replaced flag mechanism with shared memory to manage the termination state, accessed and updated by the handler function.
    - Implemented proper memory cleanup after process termination using shared memory resources.
    - Used `set_should_exit` in the signal handler to ensure graceful termination of processes and threads.

--- src/main.c
+++ src/main.c
@@ -7,45 +7,54 @@
 #include <signal.h>     
 #include "cashier.h"
 #include "customer.h"
+#include "shared_memory.h"
 
-#define NUM_CASHIERS 3
-#define NUM_CUSTOMERS 5
 
-// Zmienna globalna do sygnalizowania zakończenia pracy
-extern int should_exit=0;
+#define MAX_CASHIERS 10 // Maksymalna liczba kasjerów
+
+#define DEFAULT_CASHIERS 2
+
+#define NUM_CUSTOMERS 15 // Liczba klientów (tymczasowo)
+
+SharedMemory* shared_mem;
 
 // Handler dla sygnału SIGINT (Ctrl+C)
 void sigint_handler(int signum) {
     printf("Otrzymano sygnał SIGINT, kończenie pracy kasjerów...\n");
-    
-    // Ustawienie flagi zakończenia
-    should_exit = 1;
-    
+
+    // Zaktualizowanie flagi zakończenia
+    set_should_exit(shared_mem, 1);
+
     terminate_all_customers();
 
-    // Kasjerzy zakończą swoje wątki po sygnale
     printf("Wszystkie procesy i wątki zostaną zakończone.\n");
 }
 
 
+
 int main() {
-    pthread_t cashier_threads[NUM_CASHIERS];
-    int cashier_ids[NUM_CASHIERS];
+    pthread_t cashier_threads[MAX_CASHIERS];
+    int cashier_ids[MAX_CASHIERS];
+
+    shared_mem = init_shared_memory();  // Przypisz wskaźnik do pamięci dzielonej
 
     // Rejestracja handlera sygnału SIGINT
     signal(SIGINT, sigint_handler);
 
     // Tworzenie kasjerów
-    init_cashiers(cashier_threads, cashier_ids, NUM_CASHIERS);
+    init_cashiers(cashier_threads, cashier_ids, DEFAULT_CASHIERS);
 
     // Tworzenie procesów klientów
-    create_customer_processes(NUM_CUSTOMERS, NUM_CASHIERS);
+    create_customer_processes(NUM_CUSTOMERS, DEFAULT_CASHIERS);
 
     // Czekanie na zakończenie procesów klientów
     wait_for_customers(NUM_CUSTOMERS);
 
     // Czekanie na zakończenie kasjerów
-    wait_for_cashiers(cashier_threads, NUM_CASHIERS);
+    wait_for_cashiers(cashier_threads, DEFAULT_CASHIERS);
+
+    //Czyszczenie pamięci dzielonej
+    cleanup_shared_memory(shared_mem);
 
     return 0;
 }
