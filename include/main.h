cde8b4058dbda3846a42eb7fc98f4263d2b3b6a3
ZuzannaZawartka <raf.zuzia7@gmail.com>
2025-01-19 15:42:20 +0100

    chore: update default times value in main.h

--- include/main.h
+++ include/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define MIN_PEOPLE_FOR_CASHIER 5  // Na każdą grupę 5 klientów przypada jeden kasjer
+#define MIN_PEOPLE_FOR_CASHIER 2  // Na każdą grupę 5 klientów przypada jeden kasjer
 #define MAX_CASHIERS 10 //maksymalna liczba kasjerów
 #define MIN_CASHIERS 2 //minimalna liczba kasjerów
 #define MAX_CUSTOMERS 100  // Maksymalna liczba klientów
@@ -9,11 +9,11 @@
 #define MIN_TIME_TO_CLIENT 0.6 //czas co jaki generuja sie klienci (Kasjer sprawdza stan klientów co 0.5 sekundy)
 #define MAX_TIME_TO_CLIENT 3
 
-#define MIN_STAY_CLIENT_TIME 1 //jak długo klient jest w sklepie
-#define MAX_STAY_CLIENT_TIME 3
+#define MIN_STAY_CLIENT_TIME 3 //jak długo klient jest w sklepie
+#define MAX_STAY_CLIENT_TIME 5
 
-#define MIN_CASHIER_OPERATION 1//czas obslugi kasjera
-#define MAX_CASHIER_OPERATION 3
+#define MIN_CASHIER_OPERATION 3//czas obslugi kasjera
+#define MAX_CASHIER_OPERATION 5
 
 #define MIN_WAIT_FIREFIGHTER_TIME 120 //po jakim czasie wywola sie strazak
 #define MAX_WAIT_FIREFIGHTER_TIME 160
